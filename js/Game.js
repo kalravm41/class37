6a981500bff6d07153ab0eb4d6b4e758cebc53fb
kalravm41 <64910029+kalravm41@users.noreply.github.com>
2020-06-17 20:03:14 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,75 @@
+class Game {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+    car1 = createSprite(displayWidth/2-200,displayHeight/2,20,40);
+    car2 = createSprite(displayWidth/2,displayHeight/2,20,40);
+    car3 = createSprite(displayWidth/+200,displayHeight/2,20,40);
+    car4 = createSprite(displayWidth/2+400,displayHeight/2,20,40);
+    allcars = [car1,car2,car3,car4];
+  }
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100)
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+      var index = 0;
+      var x = 0;
+      var y;
+     // var display_position = 130;
+      for(var plr in allPlayers){
+        index = index + 1;
+        x = x+200;
+        y = displayHeight - allPlayers[plr].distance;
+        allcars[index-1].x = x;
+        allcars[index-1].y = y;
+        if(index === player.index){
+          allcars[index-1].shapeColor = "red";
+          camera.position.x = displayWidth/2
+          camera.position.y = allcars[index-1].y
+        }
+        // if (plr === "player" + player.index)
+        //   fill("red")
+        // else
+        //   fill("black");
+
+        // display_position+=20;
+        // textSize(15);
+        // text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      }
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=50
+      player.update();
+    }
+  }
+  drawSprites();
+}
